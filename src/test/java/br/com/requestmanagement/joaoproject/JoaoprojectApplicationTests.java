ca834e0fbe714a20bc4509adb30ba81006de3e26
João Vitor <joaovitorvlf95@gmail.com>
2026-05-15 20:51:54 -0300

    First commit

---
+++ src/test/java/br/com/requestmanagement/joaoproject/JoaoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.requestmanagement.joaoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
